fb2a7ad5f6df1ea6c1e1594380926deba5a5ca14
vid58 <40040775+vid58@users.noreply.github.com>
2020-02-09 22:09:36 -0500

    0w0

---
+++ anvance.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+
+/*int main(){
+    string c;
+    cout<<"Escriba: ";cin>>c;
+
+    if(c== "mother"){
+        cout<<"mutter";
+    }
+    else if(c== "father"){
+        cout<<"vater";
+    }
+    else if(c== "grand"){
+        cout<<"gross";
+    }
+    else if(c== "great"){
+        cout<<"ur";
+    }
+    else{
+        cout<<"sin info";
+    }
+
+}*/
+
+/*int main(){
+    char A[6]={'m','o','t','h','e','r'};
+    string cadena[2];
+
+    cadena[0]= A;
+    cout<<cadena[0];
+
+    cout<<endl<<"........."<<endl;
+
+    string algo="holapaco";
+    char B[4];
+    string almacen1;
+
+    for(int i=0;i<4;i++){
+        B[i]=algo[i];
+    }
+    for(int i=0;i<4;i++){
+        cout<<B[i]<<" ";
+    }
+    cout<<endl;
+
+    almacen1=B;
+
+    cout<<endl<<almacen1[3];
+}*/
+
+int main(){
+    string palabra;
+    int n;
+
+    palabra="grandmother";
+
+    for(int i=0;i<palabra.size();i++){
+        cout<<palabra[i]<<"  ";
+        n=i;
+    }
+
+    cout<<endl;
+    n++;
+    cout<<n<<endl;
+    cout<<palabra[4]<<endl;
+
+    cout<<":::::::::::::::"<<endl;
+
+    int cant_palabras;
+    cant_palabras=n/5;
+
+    cout<<cant_palabras<<endl;
+    cout<<palabra[0]<<endl;
+
+    string palabritas[cant_palabras];
+
+    if(n==6){
+        if(palabra[0]=='f'){
+            palabritas[0]=palabra;
+        }else if(palabra[0]=='m'){
+            palabritas[0]=palabra;
+        }
+    }
+    else if(n>6){
+        if(palabra[4]=='d'){
+            int letra=5;
+            int temp_letra=5;
+            char temp[temp_letra];
+            string tempo;
+            int _i=0;
+
+            for(int j=0;j<cant_palabras;j++) {
+
+                int k=0;
+                for (int i = _i; i < letra; i++) {
+
+                    temp[k] = palabra[i];
+                    tempo = temp;
+                    _i=i;
+                    k++;
+
+                }
+
+                letra=letra+_i+1+1;
+                _i++;
+                palabritas[j]=tempo;
+
+            }
+
+        }else if(palabra[4]=='t'){
+
+        }
+    }
+
+    for(int i=0;i<2;i++){
+        cout<<palabritas[i]<<" ";
+    }
+}
